880aac133e99054b42e5641ebf946aa037952543
Usmi Mukherjee <35424412+usmimukherjee@users.noreply.github.com>
2024-03-07 14:53:13 -0400

    added more explanation to the code

--- src/dialog_disentanglement.py
+++ src/dialog_disentanglement.py
@@ -16,8 +16,11 @@ cache = {}
 common_short_names = {"ng", "_2", "x_", "rq", "\\9", "ww", "nn", "bc", "te", 
                           "io", "v7", "dm", "m0", "d1", "mr", "x3", "nm", "nu", "jc", "wy", "pa", "mn",
                           "a_", "xz", "qr", "s1", "jo", "sw", "em", "jn", "cj", "j_"}
-    
+
 def initializing():
+    ''' This method to set up command-line argument parsing. 
+    It defines various parameters for training or testing that can be 
+    passed to a script from the command line, '''
     # General arguments
     parser.add_argument('prefix', help="Start of names for files produced.")
 
@@ -55,12 +58,15 @@ def initializing():
     parser.add_argument('--clip', default=3.740, type=float, help="Gradient clipping.")
 
 def header(args, out=sys.stdout):
+    ''' This method is used to print the header of the output file.'''
     head_text = "# "+ time.ctime(time.time())
     head_text += "\n# "+ ' '.join(args)
     for outfile in out:
         print(head_text, file=outfile)
         
 def update_user(users, user):
+    '''This function updates the set of users with the provided username 
+    if it's not a reserved keyword or composed only of digits.'''
     if user in reserved:
         return
     all_digit = True
@@ -72,6 +78,9 @@ def update_user(users, user):
     users.add(user.lower())
 
 def update_users(line, users):
+    ''' This method processes a line of text to extract and update user information 
+    in the provided users collection. It filters out non-user-related entries and handles
+    different formats of user names.'''
     if len(line) < 2:
         return
     user = line[1]
@@ -106,6 +115,9 @@ def update_users(line, users):
         update_user(users, core)
 
 def get_targets(line, users):
+    '''This function extracts a set of target usernames 
+    from a line of text, ensuring they are actual users 
+    and not punctuation or short, irrelevant tokens.'''
     targets = set()
     for token in line[2:]:
         token = token.lower()
@@ -132,6 +144,10 @@ def get_targets(line, users):
     return targets
 
 def lines_to_info(text_ascii):
+    ''' This function processes chat log lines to 
+    extract structured information including users, timestamps, and message targets
+    It keeps track of user interactions 
+    and message sequencing for analysis and feature extraction.'''
     users = set()
     for line in text_ascii:
         update_users(line, users)
@@ -174,6 +190,9 @@ def lines_to_info(text_ascii):
     return info, target_info
 
 def get_time_diff(info, a, b):
+    '''This function calculates the time difference in minutes between two messages, 
+    identified by their indices (a and b) in the
+    info list.'''
     if a is None or b is None:
         return -1
     if a > b:
@@ -191,11 +210,18 @@ def get_time_diff(info, a, b):
     return (60 - amin) + bmin + 60*(bhour - ahour - 1)
 
 def get_features(name, query_no, link_no, text_ascii, text_tok, info, target_info, do_cache):
+    '''feature extraction function that takes a name, query_no, link_no, text_ascii,
+    text_tok, info, target_info, and do_cache as input and returns a list of features.'''
+    # Check if the features for this combination of name, query_no, and link_no are cached,
+    # if so return the cached result to avoid recomputation.
     if (name, query_no, link_no) in cache:
         return cache[name, query_no, link_no]
 
     features = []
-
+    
+    # Extract information for the query and the link from the 'info' structure based on their indices.
+    # The 'info' structure seems to contain various details about each message, such as user information,
+    # target users, timestamp, system message flag, bot flag, and neighboring message details.
     quser, qtargets, qhour, qmin, qsystem, qis_bot, qlast_from_user, qline, qnext_from_user = info[query_no]
     luser, ltargets, lhour, lmin, lsystem, lis_bot, llast_from_user, lline, lnext_from_user = info[link_no]
 
@@ -362,37 +388,54 @@ def get_features(name, query_no, link_no, text_ascii, text_tok, info, target_inf
     return final_features
 
 def read_data(filenames, is_test=False):
+    # Initialize a list to store instances and a set to track processed file names.
     instances = []
     done = set()
+
+    # Loop over each file name to process the data.
     for filename in filenames:
+        # Extract the base name of the file by removing known file type endings.
         name = filename
         for ending in [".annotation.txt", ".ascii.txt", ".raw.txt", ".tok.txt"]:
             if filename.endswith(ending):
                 name = filename[:-len(ending)]
+        # Skip processing if this file has already been processed.
         if name in done:
             continue
+        # Mark this file as processed.
         done.add(name)
-        text_ascii = [l.strip().split() for l in open(name +".ascii.txt")]
+        # Read and split the ASCII text data into a list of words.
+        text_ascii = [l.strip().split() for l in open(name + ".ascii.txt")]
+        # Initialize an empty list to store tokenized text.
         text_tok = []
-        for l in open(name +".tok.txt"):
+        # Read, clean, and tokenize the text data.
+        for l in open(name + ".tok.txt"):
             l = l.strip().split()
+            # Remove sentence end token if present.
             if len(l) > 0 and l[-1] == "</s>":
                 l = l[:-1]
+            # Ensure that each line starts with a sentence start token.
             if len(l) == 0 or l[0] != '<s>':
                 l.insert(0, "<s>")
             text_tok.append(l)
+        # Convert lines of text to structured info format.
         info, target_info = lines_to_info(text_ascii)
 
+        # Initialize an empty dictionary to store link annotations.
         links = {}
+        # Populate links with empty lists for test data or read from annotation file for training data.
         if is_test:
             for i in range(args.test_start, min(args.test_end, len(text_ascii))):
                 links[i] = []
         else:
-            for line in open(name +".annotation.txt"):
+            # Read annotations and store them in the links dictionary.
+            for line in open(name + ".annotation.txt"):
                 nums = [int(v) for v in line.strip().split() if v != '-']
                 links.setdefault(max(nums), []).append(min(nums))
+        # Create instances by combining file name, link indices, and the processed text and info.
         for link, nums in links.items():
-            instances.append((name +".annotation.txt", link, nums, text_ascii, text_tok, info, target_info))
+            instances.append((name + ".annotation.txt", link, nums, text_ascii, text_tok, info, target_info))
+    # Return the list of compiled instances.
     return instances
 
 def simplify_token(token):
@@ -406,9 +449,12 @@ def simplify_token(token):
     return ''.join(chars)
 
 class PyTorchModel(nn.Module):
+    ''' a simplle feed forward neural network model 
+    with hidden layers and a nonlinearity function, 
+    a dropout layer for regularization, 
+    and a final output layer to produce predictions'''
     def __init__(self, input_size, hidden_size, output_size, num_layers, dropout_rate, nonlin):
         super(PyTorchModel, self).__init__()
-
         # Create word embeddings if provided
         self.embeddings = None
         if args.word_vectors:
@@ -444,19 +490,20 @@ class PyTorchModel(nn.Module):
         self.nonlin = nonlin
 
     def forward(self, features, word_ids=None):
-        if self.embeddings:
-            # Word_ids are provided as a list of lists for each option
-            # Get word embeddings for each option
-            word_embeddings = [self.embeddings(torch.tensor(ids)) for ids in word_ids]
-            # Compute maximum and mean of word embeddings for query and option
-            qvec_max = torch.max(word_embeddings[0], dim=0)[0]
-            qvec_mean = torch.mean(word_embeddings[0], dim=0)
-            ovec_max = torch.max(word_embeddings[1], dim=0)[0]
-            ovec_mean = torch.mean(word_embeddings[1], dim=0)
-            # Concatenate them with features
-            features = torch.cat([features, qvec_max, qvec_mean, ovec_max, ovec_mean], dim=0)
-        else:
-            features = torch.tensor(features, dtype=torch.float32)
+        if self.embeddings and word_ids is not None:
+            # Convert list of word_ids to a tensor
+            word_ids_tensor = torch.tensor(word_ids, dtype=torch.long).to(features.device)
+            
+            # Get word embeddings
+            word_embeddings = self.embeddings(word_ids_tensor)
+            
+            # Compute maximum and mean of word embeddings across the sequence dimension
+            qvec_max, _ = torch.max(word_embeddings, dim=1)
+            qvec_mean = torch.mean(word_embeddings, dim=1)
+            
+            # Concatenate them with features along the second dimension (features dimension)
+            features = torch.cat([features, qvec_max, qvec_mean], dim=1)
+        
         # Apply dropout to the input features
         h = self.dropout(features)
         
@@ -464,7 +511,7 @@ class PyTorchModel(nn.Module):
         for layer in self.hidden_layers:
             h = layer(h)
             h = self.apply_nonlinearity(h)
-
+        
         # Output layer
         h = self.output_layer(h)
         return h
